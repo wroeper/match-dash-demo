a52402d67f6983f57ec33093c62e45134c569a9f
Wilfried Roeper <mail@wroeper.de>
2019-02-01 14:41:48 +0100

    even better slogan

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Welcome to the jungle. we want to deploy
+            Welcome to the jungle. we want to deploy again.
           </p>
           
         </header>
